2e259836ce07dffce2ac4218f8802dbf08bdb996
ishan <ishan@ubuntu>
2013-09-25 13:29:06 +0530

    GraphML Reader Completed Using Boost

--- MiniProject/GWProcedure.cpp
+++ MiniProject/GWProcedure.cpp
@@ -1,11 +1,64 @@
-#include<iostream>
+#include <boost/graph/graphml.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graph_traits.hpp>
+#include <iostream>
+#include<fstream>
 
-using namespace std;
 
-int main(){
-	cout<<"hi";
-	cout<<"bye";
-	cout<<"da";
-	cout<<"amd";
+class Vertex {
+public:
+	std::string color;
+
+	Vertex(){
+		color = "";
+	}
+
+	Vertex(std::string s){
+		color = s;
+	}
+
+	std::string get(){
+		return color;
+	}
+};
+
+class Edge {
+public:
+	std::string name;
+	double weight;
+
+	Edge(){
+		name = "";
+		weight = 0.0;
+	}
+
+	Edge(std::string s, double d){
+		name = s;
+		weight = d;
+	}
+};
+
+typedef boost::adjacency_list<boost::vecS,boost::vecS,boost::undirectedS, Vertex,Edge> Graph;
+typedef boost::graph_traits<Graph>::vertex_descriptor CVertex;
+typedef boost::graph_traits<Graph>::edge_descriptor CEdge;
+
+int main() {
+	Graph g;
+	std::ifstream infile;
+	infile.open("test.gml",std::ios::in);
+	boost::dynamic_properties dp;
+	dp.property("color", boost::get(&Vertex::color, g));
+	dp.property("weight", boost::get(&Edge::weight,g));
+	boost::read_graphml(infile,g,dp);
+	infile.close();
+	boost::graph_traits<Graph>::edge_iterator e = boost::edges(g).first;
+	e++;
+	e++;
+	e++;
+	e++;
+	e++;
+	e++;
+	CEdge tp = *e;
+	std::cout<<g[tp].name;
 	return 0;
 }
